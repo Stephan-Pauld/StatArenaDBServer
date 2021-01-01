df05d171b7626f45eb1a750e02e9e89fd2825504
Stephan Paul <stephanpauld@gmail.com>
2020-12-31 22:17:52 -0700

    Server Overhaul Add dB Add Queries Add New EndPoints

--- routes/stats.js
+++ routes/stats.js
@@ -6,7 +6,7 @@ const API = require('call-of-duty-api')({ platform: "acti" });
 const cors = require('cors')
 const redis = require('redis');
 const port = process.env.PORT || 3030
-const bodyParser = require ('body-parser');
+const bodyParser = require('body-parser');
 const sqlConnection = require('../lib/db.js');
 const client = redis.createClient(process.env.REDIS_URL);
 
@@ -16,8 +16,8 @@ app.use(express.static("public"));
 app.use(cors());
 app.use(express.json());
 
-
 function cache(key) {
+	console.log("Checking");
 	if (key === "-guns") {
 		return function (req, res, next) {
 			const { username } = req.params;
@@ -32,15 +32,33 @@ function cache(key) {
 				}
 			})
 		}
-	} else if (key === "-achievements") {
+	} else if (key === "-stats") {
 		return function (req, res, next) {
 			const { username } = req.params;
 			client.get(`${username}${key}`, (err, data) => {
 				if (err) throw err;
 
 				if (data !== null) {
-					console.log("Grabbing Cached for Achieves!!");
-					res.send(JSON.parse(data))
+					console.log("Grabbing Cached for Stats!!");
+
+					// MAYBE SEND DATA TO REACT LIKE THIS?? SEEMS MUCH CLEANER ON THE REACT SIDE? CAN CREATE HELPER FUNCTIONS IN THE SERVER PERHAPS TOO??? ALSO WE SHOULD DO THIS BEFORE THE CACHE!
+
+					// const gamesPlayed = allData.lifetime.all.properties.gamesPlayed
+					// const misses = allData.lifetime.all.properties.misses
+					// const bestKills = allData.lifetime.all.properties.bestKills
+
+					// const newObj = {
+					// 	gamesPlayed,
+					// 	misses,
+					// 	bestKills
+					// }
+
+
+
+					const allData = JSON.parse(data);
+					const gameModes = allData.lifetime.mode
+
+					res.send(gameModes)
 				} else {
 					next()
 				}
@@ -49,8 +67,24 @@ function cache(key) {
 	}
 }
 
-Router.get("/moho", cache("-guns"), (req, res) => {
-	async function getData() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Router.get("/:username", cache("-guns"), (req, res) => {
+	async function getGunData() {
 		try {
 			await API.login(EMAIL, PASSWORD); // need usersname and pass from https://www.callofduty.com/
 			console.log("Logging In");
@@ -62,7 +96,7 @@ Router.get("/moho", cache("-guns"), (req, res) => {
 		}
 		try {
 			let data = await API.MWwz(USERNAME, PLATFORM);
-			console.log("Sending Data!!!");
+			console.log("Sending Gun Data!!!");
 
 			const { username } = req.params
 
@@ -77,27 +111,71 @@ Router.get("/moho", cache("-guns"), (req, res) => {
 			console.log(error);
 		}
 	}
-	getData()
+	getGunData()
 });
 
-// Router.get("/achieve/:username", cache("-achievements"), (req, res) => {
-  
-// 	async function getAchievements() {
-// 		try {
-// 			console.log("Sending acheives");
-// 			const { username } = req.params
-// 			const achievements = JSON.stringify(achievementObj)
-// 			console.log(username);
-// 			client.setex(`${username}-achievements`, 3600, achievements)
 
-// 			res.send(achievements)
-// 		} catch (error) {
 
-// 			console.log("Data Error");
-// 			console.log(error);
-// 		}
-// 	}
-// 	getAchievements()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Router.get("/allstats/:username", cache("-stats"), (req, res) => {
+	async function getAllData() {
+		try {
+			await API.login(EMAIL, PASSWORD); // need usersname and pass from https://www.callofduty.com/
+			console.log("Logging In");
+		} catch (Error) {
+			//Handle Exception
+			console.log("Login Error");
+
+			console.log("error");
+		}
+		try {
+			let data = await API.MWwz(USERNAME, PLATFORM);
+			console.log("Sending All Data!!!");
+
+			const { username } = req.params
+
+			const allData = JSON.stringify(data)
+			console.log(username);
+
+			client.setex(`${username}-stats`, 3600, allData)
+
+			res.send(allData)
+		} catch (error) {
+			console.log("Data Error");
+			console.log(error);
+		}
+	}
+	getAllData()
+});
+
+// Router.get("/allstats", (req, res) => {
+// 	console.log("Cats");
+// 	res.send("Toast")
 // });
 
 module.exports = Router
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
+
 const { REDIS_PORT, EMAIL, PASSWORD, USERNAME, PLATFORM } = require("./consts");
-const API = require('call-of-duty-api')({ platform: "acti" });
-const express = require("express");
-const cors = require('cors')
-const redis = require('redis');
-const port = process.env.PORT || 3030
-const bodyParser = require ('body-parser');
+
+const API 				= require('call-of-duty-api')({ platform: "acti" });
+const express 		= require("express");
+const cors 				= require('cors')
+const redis 			= require('redis');
+const port	 			= process.env.PORT || 3030
+const bodyParser 	= require ('body-parser');
 const sqlConnection = require('./lib/db.js');
 
 const app = express();
@@ -14,21 +16,15 @@ app.use(express.static("public"));
 app.use(cors());
 app.use(express.json());
 
-
-const usersRoutes = require("./routes/users");
-const trackedStatsRoutes = require("./routes/trackedStats");
-const achievementsRoutes = require("./routes/achievements");
-const statRoutes         = require("./routes/stats");
+const usersRoutes 	     	= require("./routes/users");
+const trackedStatsRoutes 	= require("./routes/trackedStats");
+const achievementsRoutes 	= require("./routes/achievements");
+const statRoutes         	= require("./routes/stats");
 
 app.use("/users", usersRoutes);
 app.use("/trackedstats", trackedStatsRoutes);
 app.use("/achievements", achievementsRoutes);
 app.use("/stats", statRoutes);
-
-
-
-
-
 
 app.listen(port, () => {
 	console.log("Example app listening on port " + port);
